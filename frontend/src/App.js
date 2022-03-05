c67a12e55f2ca0b1c2ef05f37550a7ad98571edf
DanoHailu <danohailu11@gmail.com>
2022-03-05 15:58:16 +0300

    completely responsive product listView component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,16 @@ import Topbar from './components/topbar/topbar';
 import ImageSlider from './components/imageSlider/imageSlider';
 import { sliderData } from './components/imageSlider/sliderData';
 import FeaturedInfo from './components/featuredInfo/featuredInfo';
+import ProductCard from './components/productCard/productCard';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Topbar />
+     <Topbar />
      <ImageSlider className="imageSliderComponent" slides={sliderData} />
      <FeaturedInfo />
+     <ProductCard className="productList" />
     
     </BrowserRouter>
   );
---
+++ frontend/src/components/productCard/productCard.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import './productCard.css'
+
+//import for the card view
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+
+
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      maxWidth: 300,
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },    
+  }));
+
+export default function ProductCard() {
+
+    const classes = useStyles(); 
+    
+  return (
+    <div className='productCard'>
+        <div className="productCardWrapper">
+        <h3 productCardTitle>Our Products</h3>
+            <div className="cardView">
+                <Card className='card'>
+                            <CardMedia
+                                className="cardImg"
+                                image="	http://bigone4.demo.towerthemes.com/image/cache/catalog/category%20thumb/accessories-170x151.jpg"
+                                title="Paella dish"
+                            />
+                            <CardContent>
+                                <Typography variant='body2' color='textSecondary' component='p'>
+                                    Product Name
+                                </Typography>
+                                <Typography variant='body2' color='textSecondary' component='p'>
+                                    Product Brand
+                                </Typography>
+                                <Typography variant='body2' color='textSecondary' component='p'>
+                                    Product Price
+                                </Typography>
+                            </CardContent>
+                            <CardActions disableSpacing>
+                                <IconButton aria-label="add to favorites">
+                                <FavoriteIcon />
+                                </IconButton>
+                                <IconButton aria-label="share">
+                                <ShareIcon />
+                                </IconButton>
+                                <button className='viewItemDetail'>View</button>                        
+                            </CardActions>   
+                        </Card>
+            </div>
+        </div>
+        
+    </div>
+  );
+}
